7ea0c622ced46697082762820b1cca7a906f5e0e
Echo Yin <echo_ybl@hotmail.com>
2023-07-13 11:10:19 -0400

    Updates

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+/*!
+* Start Bootstrap - Portfolio v1.0.1 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 undefined
+* Licensed under MIT (https://github.com/StartBootstrap/bootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    const typed = document.body.querySelector('.typed')
+    if (typed) {
+        let typed_strings = typed.getAttribute('data-typed-items')
+        typed_strings = typed_strings.split(',')
+        new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+        });
+    }
+
+    // Initialize animation on scroll scripts
+    AOS.init();
+
+    // Initialize isotope animations
+    var elem = document.querySelector('.hcf-isotope-grid');
+    var iso = new Isotope( elem, {
+    // options
+    itemSelector: '.hcf-isotope-item',
+    layoutMode: 'fitRows'
+    });
+
+});
